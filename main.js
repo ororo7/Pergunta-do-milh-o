a11906640b8d044dd45053d0143677b0e0450d3e
ororo7 <128612640+ororo7@users.noreply.github.com>
2024-05-13 08:20:51 -0300

    kfhuquhq

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function verificarResposta(resposta) {
     var audioAcerto = new Audio('som/silvio-santos-certa-resposta.mp3');
     var audioErro = new Audio('som/silvio-santos-que-pena-voce-errou.mp3');
 
-    if (resposta === 3) {
+    if (resposta === 4) {
         document.getElementById('resultado').innerText = 'Acertou!';
         audioAcerto.play();
     } else {
